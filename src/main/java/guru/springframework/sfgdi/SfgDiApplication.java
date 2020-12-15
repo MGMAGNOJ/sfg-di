fef8c8f70fe5791cd030140b17c6804496ee620a
mario.magno <mgmagnoj@gmail.com>
2020-12-15 13:12:02 -0300

    32VSCode

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -2,12 +2,25 @@ package guru.springframework.sfgdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.sfgdi.controllers.MyController;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+	public static void main(String[] args) 
+	
+	{
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greetings = myController.SayHello();
+
+		System.out.println(greetings);
+
+		//SpringApplication.run(SfgDiApplication.class, args);
 	}
 
 }
