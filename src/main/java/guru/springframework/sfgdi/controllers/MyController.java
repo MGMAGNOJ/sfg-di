be5d45b9292524568e83d5162e8a2660c5191cc8
mario.magno <mgmagnoj@gmail.com>
2020-12-16 11:30:00 -0300

    Logger!!!

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -1,11 +1,20 @@
 package guru.springframework.sfgdi.controllers;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
+
+
 
 @Controller
 public class MyController {
+
+    private static final Logger logger = Logger.getLogger( MyController.class.getName() );
+
     public String SayHello(){
-        System.out.println("Hello!");
+        
+        logger.log(Level.INFO,"Hello I'm Controller!!!");
         return "Good Bye!";
     }
 }
