3f2b5919461ed2959e5d2294c288daca70c0f8ef
mario.magno <mgmagnoj@gmail.com>
2021-02-14 13:07:32 -0300

    Ok

--- src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -1,7 +1,5 @@
 package guru.springframework.sfgdi.config;
 
-import javax.print.FlavorException;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/guru/springframework/sfgdi/config/Sfgdi.java
+++ src/main/java/guru/springframework/sfgdi/config/Sfgdi.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import guru.springframework.sfgdi.services.GreetingService;
 
 @Configuration
 public class Sfgdi {
