e1db2bb009cbe77db067fac24cebebae444a1276
mario.magno <mgmagnoj@gmail.com>
2021-02-14 13:22:14 -0300

    Ok

--- src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -1,16 +1,21 @@
 package guru.springframework.sfgdi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import guru.springframework.examplebeans.FakeDatasourse;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${username}")
     String username;
@@ -21,10 +26,15 @@ public class PropertyConfig {
     @Value("${url}")
     String url;
 
+    
+
     @Bean
     public FakeDatasourse fakeDatasourse(){
         FakeDatasourse fakeDatasourse = new FakeDatasourse();
-        fakeDatasourse.setUsername(username);
+        //fakeDatasourse.setUsername(username);
+        
+        fakeDatasourse.setUsername(env.getProperty("USERNAME"));
+
         fakeDatasourse.setPassword(password);
         fakeDatasourse.setUrl(url);
         return fakeDatasourse;
